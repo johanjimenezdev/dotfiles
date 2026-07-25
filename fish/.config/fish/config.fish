bc4bb6c947135d33d5ee8c834565ed7d6622f8b9
johanjimenezdev <johanjimenezdev@gmail.com>
2026-07-25 02:06:08 -0500

    feat(fish): add pnpm path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -16,8 +16,13 @@ alias ff fastfetch
 # -- Editor --------------------------------------------------
 set -gx EDITOR nvim
 
-# -- Path ----------------------------------------------------
+# -- Paths ---------------------------------------------------
 export PATH="$HOME/.local/bin:$PATH"
+
+set -gx PNPM_HOME "/home/johan/.local/share/pnpm"
+if not string match -q -- "$PNPM_HOME/bin" $PATH
+    set -gx PATH "$PNPM_HOME/bin" $PATH
+end
 
 # -- Zoxide --------------------------------------------------
 zoxide init fish | source
